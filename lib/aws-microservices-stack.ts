439f4eb29754e0623a6b1505ba88012832293fe6
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-01-29 12:17:41 +0200

    Update aws-microservices-stack.ts

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,10 +1,5 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { join } from 'path';
 import { SwnApiGateway } from './apigateway';
 import { SwnDatabase } from './database';
 import { SwnMicroservices } from './microservice';
